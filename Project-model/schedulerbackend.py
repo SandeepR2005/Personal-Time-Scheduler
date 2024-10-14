81ee4e9d54b46ccaa8b2e2b0eb6be06ca4490c98
SandeepR2005 <sandeepramamoorthy2005@gmail.com>
2024-10-14 22:24:14 +0530

    Commit

---
+++ Project-model/schedulerbackend.py
@@ -0,0 +1,8 @@
+def schedule_date():
+    date=[]
+    schedule={}
+    a=intput("Enter the Date on which you need to make schedule")
+    date.append(a)
+    b=input("Enter the schedule time and priority")
+    p=input()
+    schedule.update(b,p)
